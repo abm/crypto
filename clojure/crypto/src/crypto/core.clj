401ae319d2b3d400e016d6eeee7d71069b4af6ee
Aaron Mayerson <aaron.mayerson@cidmcorp.com>
2014-01-10 16:48:28 -0800

    working Base64 implementation

--- clojure/crypto/src/crypto/core.clj
+++ clojure/crypto/src/crypto/core.clj
@@ -11,9 +11,34 @@
   (let [s (Integer/toBinaryString i)]
     (str (apply str (repeat (- 8 (count s)) "0")) s)))
 
+(defn b64-indexes
+  "Convert bytes to their Base64 index values"
+  [bs]
+  (->> bs
+   (map bit-str)
+   (clojure.string/join)
+   (partition 6 6 (repeat \0))
+   (map clojure.string/join)
+   (map #(Integer/parseInt %1 2))))
+
+(def b64-table ["A" "B" "C" "D" "E" "F" "G" "H" "I" "J" "K" "L" "M" "N" "O" "P" "Q" "R" "S" "T" "U" "V" "W" "X" "Y" "Z" "a" "b" "c" "d" "e" "f" "g" "h" "i" "j" "k" "l" "m" "n" "o" "p" "q" "r" "s" "t" "u" "v" "w" "x" "y" "z" "0" "1" "2" "3" "4" "5" "6" "7" "8" "9" "+" "/"])
+
+(defn b64-lookup
+  [indexes]
+  (map #(nth b64-table %1) indexes))
+
+(defn b64-pad
+  [cs]
+  (let [s (clojure.string/join cs)]
+    (str s (apply str (repeat (- 4 (count s)) "=")))))
+
 (defn b64
   "Convert a string into Base64"
   [bs]
   (->> bs
    (hexify)
-   (partition 3 3 (repeat 0))))
+   (partition 3 3 [])
+   (map b64-indexes)
+   (map b64-lookup)
+   (map b64-pad)
+   (clojure.string/join)))
--- clojure/crypto/test/crypto/core_test.clj
+++ clojure/crypto/test/crypto/core_test.clj
@@ -13,18 +13,18 @@
  (bit-str 97) => "01100001"
  (bit-str 110) => "01101110")
 
-;; (fact
-;;  "convert byte seq to 24 bit partitions"
-;;  (p24 [] => 0)
-;;  (p24 [77] => 2r1001101)
-;;  (p24 [77 97 110]) => 2r 01001101 01100001 01101110)
+(fact
+ (b64-indexes []) => []
+ (b64-indexes [3]) => [0 48]
+ (b64-indexes [3 217]) => [0 61 36]
+ (b64-indexes [77 97 110]) => [19 22 5 46])
 
-;; (fact
-;;  "encode hex sequences to Base64"
-;;  (b64 "") => ""
-;;  (b64 "f") => "Zg=="
-;;  (b64 "fo") => "Zm8="
-;;  (b64 "foo") => "Zm9v"
-;;  (b64 "foob") => "Zm9vYg=="
-;;  (b64 "fooba") => "Zm9vYmE="
-;;  (b64 "foobar") => "Zm9vYmFy")
+(fact
+ "encode hex sequences to Base64"
+ (b64s "") => ""
+ (b64s "f") => "Zg=="
+ (b64s "fo") => "Zm8="
+ (b64s "foo") => "Zm9v"
+ (b64s "foob") => "Zm9vYg=="
+ (b64s "fooba") => "Zm9vYmE="
+ (b64s "foobar") => "Zm9vYmFy")
